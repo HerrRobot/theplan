b75ff402dfb858a2cf0a38fbac962e346544e3f4
Andrei C. Ionescu <54107685+Machinamaniac@users.noreply.github.com>
2023-01-23 16:15:45 +0100

    Added div for behavioral stats
    - top of the page
    - CSS for it in form.css
    
    Will be made hidden once we are done formating it.

---
+++ behavioral_tracking.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    init();
+    
+}
+
+function init() {
+    
+}
+
+function AndreiMethods() {
+    /*
+    1. Mouse clicks
+    2. Key presses on fields
+    */
+
+}
